961dd60ca2ce4fcb30fe54f2a4404104b459207d
nguyenlongcs <thanhlong86.tt@gmail.com>
2025-12-22 15:30:32 +0700

    index + req.txt

---
+++ frontend/index.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('pages/home.html')
+
+@app.route('/about')
+def about():
+    return render_template('pages/about.html')
+
+@app.route('/services')
+def services():
+    return render_template('pages/services.html')
+
+@app.route('/feedback')
+def feedback():
+    return render_template('pages/feedback.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('pages/contact.html')
+
+@app.route('/booking')
+def booking():
+    return render_template('pages/booking.html')
+
+if __name__ == "__main__":
+    app.run(
+        host="127.0.0.1",
+        port=5500,
+        debug=True   # auto reload HTML + Python
+    )
