b4adb72ff7572d34dd8bb4f84255c35d9aa24f74
nguyenlongcs <thanhlong86.tt@gmail.com>
2025-12-30 21:33:46 +0700

    Backend V14.0 - Role

--- dao/account_dao.py
+++ dao/account_dao.py
@@ -4,7 +4,7 @@ Data Access Object cho Account
 """
 from datetime import datetime
 from __init__ import db
-from models import Account
+from models import Account, Customer, Employee
 
 
 def get_account_by_username(username):
@@ -12,13 +12,16 @@ def get_account_by_username(username):
     return Account.query.filter_by(username=username).first()
 
 
-def create_account(account_data, customer_id):
+def create_account(account_data, customer_id=None):
     """Tạo tài khoản mới"""
     account = Account(
         accountId=account_data['accountId'],
         username=account_data['username'],
         passwordHash=account_data['passwordHash'],
         role=account_data['role'],
+        fullName=account_data.get('fullName'),
+        phone=account_data.get('phone'),
+        email=account_data.get('email'),
         customerId=customer_id,
         createdAt=datetime.now()
     )
@@ -36,6 +39,7 @@ def update_account_password(username, new_password_hash):
         return True
     return False
 
+
 def get_all_accounts():
     """Lấy tất cả tài khoản"""
     return Account.query.all()
@@ -49,3 +53,30 @@ def update_account_role(username, new_role):
         db.session.commit()
         return True
     return False
+
+
+def get_account_info_by_role(account):
+    """Lấy thông tin account theo role với data từ account table"""
+    base_info = {
+        'accountId': account.accountId,
+        'username': account.username,
+        'role': account.role,
+        'name': account.fullName,
+        'phone': account.phone,
+        'email': account.email,
+        'createdAt': account.createdAt.isoformat()
+    }
+    
+    if account.role == 'Customer' and account.customer and account.customer.active:
+        base_info['customerId'] = account.customer.customerId
+        base_info['loyaltyPoints'] = account.customer.loyaltyPoints
+        base_info['membershipLevel'] = account.customer.membershipLevel
+    elif account.role == 'Employee' and account.employee and account.employee.active:
+        base_info['employeeId'] = account.employee.employeeId
+        base_info['position'] = account.employee.position
+        base_info['department'] = account.employee.department
+    elif account.role == 'Cashier' and account.employee and account.employee.active:
+        base_info['employeeId'] = account.employee.employeeId
+        base_info['position'] = 'Thu ngân'
+    
+    return base_info
--- dao/customer_dao.py
+++ dao/customer_dao.py
@@ -3,26 +3,27 @@
 Data Access Object cho Customer
 """
 from __init__ import db
-from models import Customer
+from models import Customer, Account
 
 
 def get_all_customers():
-    """Lấy danh sách tất cả khách hàng"""
-    return Customer.query.all()
+    """Lấy danh sách tất cả khách hàng active với thông tin từ account"""
+    customers = Customer.query.filter_by(active=True).all()
+    return customers
 
 
 def get_customer_by_id(customer_id):
     """Lấy thông tin khách hàng theo ID"""
-    return Customer.query.get(customer_id)
+    return Customer.query.filter_by(customerId=customer_id, active=True).first()
 
 
 def create_customer(data):
     """Tạo khách hàng mới"""
     customer = Customer(
         customerId=data['customerId'],
-        name=data['name'],
-        phone=data['phone'],
-        email=data.get('email', '')
+        loyaltyPoints=data.get('loyaltyPoints', 0),
+        membershipLevel=data.get('membershipLevel', 'Basic'),
+        active=True
     )
     db.session.add(customer)
     db.session.commit()
@@ -30,21 +31,32 @@ def create_customer(data):
 
 
 def update_customer(customer_id, data):
-    """Cập nhật thông tin khách hàng"""
-    customer = Customer.query.get(customer_id)
-    if customer:
-        customer.name = data.get('name', customer.name)
-        customer.phone = data.get('phone', customer.phone)
-        customer.email = data.get('email', customer.email)
+    """Cập nhật thông tin khách hàng - cập nhật vào account"""
+    customer = Customer.query.filter_by(customerId=customer_id, active=True).first()
+    if customer and customer.account:
+        account = customer.account
+        if 'name' in data:
+            account.fullName = data['name']
+        if 'phone' in data:
+            account.phone = data['phone']
+        if 'email' in data:
+            account.email = data['email']
+
+        # Cập nhật thông tin đặc thù customer
+        if 'loyaltyPoints' in data:
+            customer.loyaltyPoints = data['loyaltyPoints']
+        if 'membershipLevel' in data:
+            customer.membershipLevel = data['membershipLevel']
+
         db.session.commit()
     return customer
 
 
 def delete_customer(customer_id):
-    """Xóa khách hàng"""
+    """Soft delete khách hàng"""
     customer = Customer.query.get(customer_id)
     if customer:
-        db.session.delete(customer)
+        customer.active = False
         db.session.commit()
         return True
     return False
